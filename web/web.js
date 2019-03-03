43781e6eeaca2e020451b221843df94d22bf3e3f
creme <maha93@gmx.de>
2019-03-03 22:24:23 +0100

    added web socket

---
+++ web/web.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+class WebSocket {
+  constructor(token, port, client) {
+    this.token = token;
+    this.client = client;
+
+    this.app = express();
+    this.app.engine('hbs', hbs({
+      extname: 'hbs',
+      defaultLayout: 'layout',
+      layoutsDir: `${__dirname}/layouts`
+    }));
+    this.app.set('views', path.join(__dirname, 'views'));
+    this.app.set('view engine', 'hbs');
+    this.app.use(express.static(path.join(__dirname, 'public')));
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(bodyParser.json());
+
+    this.registerRoutes();
+
+    this.server = this.app.listen(port, () => {
+      console.log(`Websocket listening on ${this.server.address().port}`);
+    });
+  }
+
+  checkToken(_token) {
+    return (_token == this.token);
+  }
+
+  registerRoutes() {
+
+    // http://localhost:port/
+    this.app.get('/', (req, res) => {
+      let _token = req.query.token;
+
+      if (!this.checkToken(_token)) {
+        res.render('error', {title: 'ERROR', errtype: 'INVALID TOKEN'});
+        return;
+      }
+
+      let channels = [];
+      this.client.guilds.first().channels
+      .filter(c => c.type == 'text')
+      .forEach(c => {
+        channels.push({id: c.id, name: c.name});
+      });
+
+      res.render('index', {
+        title: 'WELCOME',
+        token: _token,
+        channels: channels
+      });
+
+    });
+
+    this.app.post('/sendMessage', (req, res) => {
+      // let _token = req.body.token;
+      let text = req.body.text;
+      let channelID = req.body.channelID;
+
+      // if (!this.checkToken(_token)) return;
+
+      let channel = this.client.guilds.first().channels.get(channelID);
+
+      if (channel) {
+        channel.send(text);
+      }
+    });
+
+  }
+
+}
+
+module.exports = WebSocket;
